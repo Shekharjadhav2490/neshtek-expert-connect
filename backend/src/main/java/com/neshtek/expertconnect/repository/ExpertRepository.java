a921e691c5b4571c0503e7d684eac363298d866a
Shekharjadhav2490 <61381696+Shekharjadhav2490@users.noreply.github.com>
2026-08-22 20:30:32 +0530

    feat: enable pageable expert search

--- backend/src/main/java/com/neshtek/expertconnect/repository/ExpertRepository.java
+++ backend/src/main/java/com/neshtek/expertconnect/repository/ExpertRepository.java
@@ -2,8 +2,9 @@ package com.neshtek.expertconnect.repository;
 
 import com.neshtek.expertconnect.entity.Expert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ExpertRepository extends JpaRepository<Expert, Long> {
+public interface ExpertRepository extends JpaRepository<Expert, Long>, JpaSpecificationExecutor<Expert> {
     boolean existsByEmailIgnoreCase(String email);
     boolean existsByMobileNumber(String mobileNumber);
 }
