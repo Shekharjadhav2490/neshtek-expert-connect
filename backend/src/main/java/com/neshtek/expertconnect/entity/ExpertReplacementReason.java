3e75d9625d118b22cdad625624be46242e29dd1a
Shekharjadhav2490 <61381696+Shekharjadhav2490@users.noreply.github.com>
2026-08-30 13:18:26 +0530

    feat: add replacement reason codes

---
+++ backend/src/main/java/com/neshtek/expertconnect/entity/ExpertReplacementReason.java
@@ -0,0 +1,11 @@
+package com.neshtek.expertconnect.entity;
+
+public enum ExpertReplacementReason {
+    SKILLS_MISMATCH,
+    QUALITY_ISSUE,
+    AVAILABILITY,
+    COMMUNICATION,
+    REQUIREMENT_CHANGED,
+    EXPERT_UNAVAILABLE,
+    OTHER
+}
