50e96faab79c0b75435d77b14912cc1154250263
Shekharjadhav2490 <61381696+Shekharjadhav2490@users.noreply.github.com>
2026-08-30 12:52:51 +0530

    feat: add customer requirement edit route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home.component').then((m) => m.HomeComponent) },
   { path: 'login', loadComponent: () => import('./pages/login.component').then((m) => m.LoginComponent) },
   { path: 'customer/dashboard', canActivate: [authGuard, roleGuard(['CUSTOMER', 'ROLE_CUSTOMER'])], loadComponent: () => import('./pages/customer-dashboard.component').then((m) => m.CustomerDashboardComponent) },
+  { path: 'customer/requirements/:requirementId/edit', canActivate: [authGuard, roleGuard(['CUSTOMER', 'ROLE_CUSTOMER'])], loadComponent: () => import('./pages/customer-requirement-edit.component').then((m) => m.CustomerRequirementEditComponent) },
   { path: 'customer/requirements/:requirementId/matches', canActivate: [authGuard, roleGuard(['CUSTOMER', 'ROLE_CUSTOMER'])], loadComponent: () => import('./pages/customer-matching.component').then((m) => m.CustomerMatchingComponent) },
   { path: 'customer/consultations', canActivate: [authGuard, roleGuard(['CUSTOMER', 'ROLE_CUSTOMER'])], loadComponent: () => import('./pages/customer-consultations.component').then((m) => m.CustomerConsultationsComponent) },
   { path: 'customer/work-logs/:engagementId', canActivate: [authGuard, roleGuard(['CUSTOMER', 'ROLE_CUSTOMER'])], loadComponent: () => import('./pages/customer-work-logs.component').then((m) => m.CustomerWorkLogsComponent) },
